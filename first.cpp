29a0223e4e4f8f79673aaa6de191d952e2b8abd8
TaoXu-HNU <528858941@qq.com>
2023-05-24 21:41:39 +0800

    0524

--- first.cpp
+++ first.cpp
@@ -6,6 +6,9 @@ int main(int argc, char **argv)
     std::cout<<"This is first files"<<endl;
     int a, b;
     b = a + 10;
+
+    string school = "HNU";
+    std::cout<<school<<endl;
     
     return 0;
 }
